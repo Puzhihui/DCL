0ee8f74cb0417c28ebef1544309e4437778c252b
Puzhihui <1617569019@qq.com>
2024-03-20 10:30:04 +0800

    update

--- run/a.服务启动.bat
+++ run/a.服务启动.bat
@@ -1,8 +1,4 @@
-title smic_server
 @echo off
-D:
-cd D:\Solution\code\smic\automatic_defect_classification_server\service
-@REM CALL D:\Anaconda3\Scripts\activate.bat
-call conda activate torch1.0_cuda8.0
-python server_smic.py
-pause
+start cmd /K "D:&&cd D:\Solution\code\smic\automatic_defect_classification_server\service&&conda activate torch1.0_cuda8.0&&title server_offline&&python server_smic.py"
+start cmd /K "D:&&cd D:\Solution\code\smic\automatic_defect_classification_server\service&&conda activate torch1.0_cuda8.0&&title server_eagle&&python server_smic_eagle.py"
+@REM pause
