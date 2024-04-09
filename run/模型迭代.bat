a60605def1a9aab85b87d899c880bdcc0c42b7ac
puzhihui <1617569019@qq.com>
2024-04-09 15:47:08 +0800

    update

--- run/模型迭代.bat
+++ run/模型迭代.bat
@@ -4,25 +4,20 @@ cd D:\Solution\code\smic\DCL\smic_tools_V2
 @REM CALL D:\Anaconda3\Scripts\activate.bat
 call conda activate torch1.0_cuda8.0
 
-set client=M6
 set imagedata=F:\ImageData
-set img_path=D:\Solution\datas\get_report
+set client=%1
+set mode=%2
+set epoch=%3
+set batch_size=%4
 
-echo Back:  train Back model
-echo Front: train Front model
-:input
-set /p userInput=Please input command, Back or Front:
 echo ==================================================
 
-@REM python stat_acc.py --mode %userInput% --img_path %img_path% --client %client%
-@REM python get_data_from_reviewed.py --mode %userInput% --img_path %img_path% --client %client%
-
 cd D:\Solution\code\smic\DCL\smic_tools
-python gen_smic_txt.py --mode %userInput% --client %client%
+python gen_smic_txt.py --mode %mode% --client %client%
 
 cd D:\Solution\code\smic\DCL\
-python train_smic.py --mode %userInput% --epoch 25 --client %client% --tb 16 --vb 16
+python train_smic.py --mode %mode% --epoch %epoch% --client %client% --tb %batch_size% --vb %batch_size%
 
 cd D:\Solution\code\smic\DCL\smic_tools
-python replace_online_model.py --mode %userInput%
+python replace_online_model.py --mode %mode%
 pause
